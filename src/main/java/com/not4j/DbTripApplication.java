766db5562a71166144d0978c188b56da6b6982f7
sphinks <sphinks@newmail.ru>
2016-05-28 18:43:45 +0300

    Init with autogenerated src

---
+++ src/main/java/com/not4j/DbTripApplication.java
@@ -0,0 +1,12 @@
+package com.not4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbTripApplication.class, args);
+	}
+}
