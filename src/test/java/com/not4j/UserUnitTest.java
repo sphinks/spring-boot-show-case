92564d16d79d92b29c6780571ac0285d9811739a
sphinks <sphinks@newmail.ru>
2016-05-31 01:39:01 +0300

    Step 7

--- src/test/java/com/not4j/UserUnitTest.java
+++ src/test/java/com/not4j/UserUnitTest.java
@@ -1,8 +1,6 @@
 package com.not4j;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.not4j.controllers.UserController;
 import com.not4j.dao.TripDao;
 import com.not4j.dao.UserDao;
 import com.not4j.model.User;
@@ -11,31 +9,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.*;
+import java.util.Arrays;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -44,27 +32,26 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 /**
  * @Author: ivan
- * Date: 28.05.16
- * Time: 20:23
+ * Date: 29.05.16
+ * Time: 0:40
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DbTripApplication.class)
-@WebIntegrationTest("server.port:8888")
-public class UserIntegrationTest {
+@WebAppConfiguration
+public class UserUnitTest {
 
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
 
-    //Test RestTemplate to invoke the APIs.
-    private RestTemplate restTemplate = new TestRestTemplate();
-
-    @Value("${local.server.port}")
-    int port;
+    private MockMvc mockMvc;
 
     private HttpMessageConverter mappingJackson2HttpMessageConverter;
 
     private User user;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
     @Autowired
     private UserDao userDao;
@@ -72,11 +59,19 @@ public class UserIntegrationTest {
     @Autowired
     private TripDao tripDao;
 
-    //Required to Generate JSON content from Java objects
-    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
+                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
+
+        Assert.assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
 
     @Before
     public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
 
         this.tripDao.deleteAllInBatch();
         this.userDao.deleteAllInBatch();
@@ -85,20 +80,25 @@ public class UserIntegrationTest {
     }
 
     @Test
-    public void testCreateBookApi() throws JsonProcessingException {
+    public void userNotFound() throws Exception {
+        mockMvc.perform(get("/george/somepath/")
+                .content(this.json(""))
+                .contentType(contentType))
+                .andExpect(status().isNotFound());
+    }
 
-        //Invoking the API
-        ResponseEntity<List<User>> apiResponse =
-                restTemplate.exchange("http://localhost:8888/user",
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
-                        });
+    @Test
+    public void readAllUsers() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.[0].userId", is(this.user.getUserId().intValue())));
+    }
 
-        assertNotNull(apiResponse);
-        assertThat( apiResponse.getStatusCode() , equalTo(HttpStatus.OK));
-        assertThat(apiResponse.getBody().size(), is(1));
-        User actualUser = apiResponse.getBody().get(0);
-        assertEquals(user.getUserId(), actualUser.getUserId());
-        assertEquals(user.getLogin(), actualUser.getLogin());
-        assertEquals(user.getUserId(), actualUser.getUserId());
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
     }
 }
